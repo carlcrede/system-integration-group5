2e61f4680ab4b8039e5495421c0793bde92967cf
mstrucka <mstrucka@gmail.com>
2022-11-22 18:29:47 +0100

    azure app services refactor start

--- graphQL/graphql/index.ts
+++ graphQL/graphql/index.ts
@@ -9,12 +9,6 @@ import { createServer } from 'http';
 import express from 'express';
 import { useServer } from 'graphql-ws/lib/use/ws';
 import { makeExecutableSchema } from '@graphql-tools/schema';
-import handler from 'azure-function-express';
-
-// TODO: the problem is that we need a WS server to run the subscriptions, however, the WS server cannot presumably be there as
-// we use the apollo-server-azure-functions and not the classic server and we therefore cannot have an express middleware and all the
-// other necessary things needed there, as in the article: https://www.apollographql.com/docs/apollo-server/data/subscriptions/ 
-// come back later to this if time
 
 setInterval(async () => {
     await db_connect();
@@ -25,26 +19,17 @@ const schema = makeExecutableSchema({ typeDefs, resolvers });
 const app = express();
 const httpServer = createServer(app);
 
-const wsServer = new WebSocketServer({
-    server: httpServer,
-    path: '/graphql',
-});
+// const wsServer = new WebSocketServer({
+//     server: httpServer,
+//     path: '/graphql',
+// });
 
-const serverCleanup = useServer({ schema }, wsServer);
+// const serverCleanup = useServer({ schema }, wsServer);
 
-const server = new ApolloServer({
-    schema,
-    csrfPrevention: true,
-});
-
-
-async function startIt() {
-    await server.start();
-}
-startIt().then(() => {
-    app.use('/graphql', expressMiddleware(server));
-    const PORT = 80;
-    httpServer.listen(PORT, () => {
-    });
-});
-export default handler.createHandler(httpServer);
+// const server = new ApolloServer({
+//     schema,
+//     csrfPrevention: true,
+// });
+httpServer.listen(3000, () => {
+    console.log("server on")
+})
