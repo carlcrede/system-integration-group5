0c0921f17a7b375077134a5398f8d995a7efd125
samlama111 <sam.horacek@gmail.com>
2022-11-29 13:31:02 +0100

    Implementation of product search

--- website/src/routes/Root.tsx
+++ website/src/routes/Root.tsx
@@ -1,9 +1,10 @@
 import { Box, Button, Center, Flex, Img, Input, InputGroup, InputRightElement, Link, SimpleGrid, Spinner, Text } from "@chakra-ui/react"
 import { useQuery, gql } from '@apollo/client';
 import { SearchIcon, StarIcon } from "@chakra-ui/icons";
+import { useState, useEffect } from "react";
 
-const SEARCH_PRODUCTS = gql`
-  query GetLocations {
+const LIST_PRODUCTS = gql`
+  query GetProducts {
       products(pageNumber: 1) {
         id,
         product_name,
@@ -18,23 +19,47 @@ const SEARCH_PRODUCTS = gql`
   }
 `;
 
-function Root() {
-  const { loading, error, data } = useQuery(SEARCH_PRODUCTS);
+const SEARCH_PRODUCTS = gql`
+  query SearchProducts($name: String!) {
+    searchProduct(name: $name) {
+        id,
+        product_name,
+        product_sub_title,
+        product_description,
+        price,
+        main_category,
+        sub_category
+        link,
+        overall_rating
+    }
+  }
+`;
 
+function Root() {
+  const [productNameInput, setProductNameInput] = useState('')
+  const { loading, error, data } = useQuery(SEARCH_PRODUCTS, {
+    variables: { name: productNameInput }
+  });
+
+  const item = data?.searchProduct 
   return (
     <Flex flexDir='column' align={'center'} justifyContent={'center'}>
         <Text fontSize='x-large'>Products</Text>
         <InputGroup size='md'>
-          <Input placeholder='Product name' />
+          <Input 
+            value={productNameInput} 
+            onChange={(event) => setProductNameInput(event.target.value)} 
+            placeholder='Product name' />
           <InputRightElement>
             <Button>
               <SearchIcon />
             </Button>
           </InputRightElement>
         </InputGroup>
-        {(loading || !data) && <Spinner />}
+        {loading && <Spinner />}
         <SimpleGrid columns={3} spacing={8} my="5">
-          {data && data.products && data.products.map((item) => (
+          {/* {data && data.products && data.products.map((item) => ( */}
+          {item && (
             <Box key={item.id} p='2' border='1px' borderColor='gray.200' borderRadius="md">
               <Text mr='auto'>{item.main_category} {">"} {item.sub_category}</Text>
               <Img my="5" />
@@ -45,7 +70,8 @@ function Root() {
                 <Text>{item.price}</Text>
               </Box>
             </Box>
-          ))}
+            )}
+          {/* ))} */}
         </SimpleGrid>
     </Flex>
   )
