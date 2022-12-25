cf5d8d244ec27bb5389340730c40ccba79dc7a2e
Kimon Kremizas <k.kremizas@gmail.com>
2022-12-25 14:16:30 +0100

    Add cors to websockets

--- authentication/server.js
+++ authentication/server.js
@@ -125,17 +125,19 @@ passport.use(Strategy);
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+    }
+});
 
 // convert a connect middleware to a Socket.IO middleware
 const wrap = middleware => (socket, next) => middleware(socket.request, {}, next);
 
-//io.use(wrap(sessionMiddleware));
 io.use(wrap(bodyParser.json()));
 io.use(wrap(cookieParser()));
 io.use(wrap(bodyParser.urlencoded({ extended: false })));
 io.use(wrap(passport.initialize()));
-//io.use(wrap(passport.session()));
 
 io.use((socket, next) => {
     if (socket.request.user) {
